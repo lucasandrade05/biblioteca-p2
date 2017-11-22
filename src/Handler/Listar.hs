00e2e3177f5e1f6ddc807716b0e655a5a1b8ab9b
romefeller <algarcia@usp.br>
2017-11-22 16:51:15 +0000

    Testando cadastro, listagem e exclusao

--- src/Handler/Listar.hs
+++ src/Handler/Listar.hs
@@ -9,6 +9,15 @@ module Handler.Listar where
 import Import
 import Database.Persist.Postgresql
 
+formLivro :: Form Livro
+formLivro = renderBootstrap $ Livro
+    <$> areq textField "Titulo: " Nothing
+    <*> areq textField "Autor: " Nothing
+    <*> areq textField "Editora: " Nothing
+    <*> areq textField "Gênero: " Nothing
+    <*> areq intField "Ano: "  Nothing
+    <*> areq intField "Estoque: " Nothing    
+
 getListarR :: Handler Html
 getListarR = do 
     livros <- runDB $ selectList [] [Asc LivroTitulo]
@@ -34,7 +43,7 @@ getListarR = do
                             <td> #{livroEditora livro}
                             <td> #{livroEstoque livro}
                             <td> 
-                                <form action=@{ApagarProdR pid} method=post>
+                                <form action=@{ApagarLivroR pid} method=post>
                                     <input type="submit" value="Deletar">
                             
         |]
@@ -44,3 +53,24 @@ postApagarLivroR :: LivroId -> Handler Html
 postApagarLivroR pid = do 
     runDB $ delete pid
     redirect ListarR
+    
+getCadLivroR :: Handler Html
+getCadLivroR = do 
+     (widget, enctype) <- generateFormPost formLivro
+     defaultLayout $ do
+        addStylesheet $ (StaticR css_bootstrap_css)
+        [whamlet|
+            <form action=@{CadLivroR} method=post enctype=#{enctype}>
+                ^{widget}
+                <input type="submit" value="Enviar">
+            
+        |]
+
+postCadLivroR :: Handler Html
+postCadLivroR = do 
+    ((result,_),_) <- runFormPost formLivro
+    case result of
+        FormSuccess livro -> do 
+            runDB $ insert livro 
+            redirect ListarR
+        _ -> redirect HomeR
