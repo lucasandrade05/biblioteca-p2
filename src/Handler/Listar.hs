3986b53424c558224a743f6d591728f899e0f0ce
romefeller <algarcia@usp.br>
2017-11-28 17:44:11 +0000

    Adicionado Pesquisar

--- src/Handler/Listar.hs
+++ src/Handler/Listar.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE TypeFamilies, DeriveGeneric #-}
 module Handler.Listar where
 
+import Control.Monad
 import Import
 import Database.Persist.Postgresql
 
@@ -16,17 +17,58 @@ formLivro = renderBootstrap $ Livro
     <*> areq textField "Editora: " Nothing
     <*> areq textField "Gênero: " Nothing
     <*> areq intField "Ano: "  Nothing
+    <*> areq textField "Sinopse: " Nothing
     <*> areq intField "Estoque: " Nothing    
+    
+
+data Pesquisa = Pesquisa
+    { pesquisa          :: Text
+    }
+
+formPesquisa :: Form Pesquisa
+formPesquisa = renderBootstrap $ Pesquisa
+    <$> areq textField "Pesquisar: " Nothing
+    
+toTexto :: Pesquisa -> Text
+toTexto (Pesquisa x) = x
 
 getListarR :: Handler Html
 getListarR = do 
-    livros <- runDB $ selectList [] [Asc LivroTitulo]
+    (widget, enctype) <- generateFormPost formPesquisa
+    livros <- runDB $ selectList [] [Asc LivroAutor, Asc LivroTitulo]
     defaultLayout $ do 
         addStylesheet $ (StaticR css_bootstrap_css)
         addScript $ StaticR js_jquery_min_js
         addScript $ StaticR js_bootstrap_min_js
+        toWidget $ $(whamletFile "templates/menu.hamlet")
         toWidget $ $(whamletFile "templates/listar.hamlet") 
         
+
+
+postPesqLivroR :: Handler Html
+postPesqLivroR = do 
+    ((result,_),_) <- runFormPost formPesquisa
+    case result of
+        FormSuccess pesquisar -> do 
+            let livro = toTexto(pesquisar)
+            redirect (BuscarLivroR livro)
+        _ -> do
+            setMessage $ [shamlet| Dados invalidos! |] 
+            redirect ListarR
+            
+getBuscarLivroR :: Text -> Handler Html
+getBuscarLivroR livro = do
+    (widget, enctype) <- generateFormPost formPesquisa
+    livros <- runDB $ selectList [Filter LivroTitulo (Left $ "%"++ livro ++"%") (BackendSpecificFilter "ILIKE")] []
+    --livros <- runDB $ selectList [LivroTitulo >. "%"++livros++"%"][Asc LivroAutor, Asc LivroTitulo]
+    defaultLayout $ do 
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addScript $ StaticR js_jquery_min_js
+        addScript $ StaticR js_bootstrap_min_js
+        toWidget $ $(whamletFile "templates/menu.hamlet")
+        toWidget $ $(whamletFile "templates/listar.hamlet") 
+
+
         
         
 postApagarLivroR :: LivroId -> Handler Html 
@@ -39,18 +81,19 @@ getCadLivroR = do
      (widget, enctype) <- generateFormPost formLivro
      defaultLayout $ do
         addStylesheet $ (StaticR css_bootstrap_css)
-        [whamlet|
-            <form action=@{CadLivroR} method=post enctype=#{enctype}>
-                ^{widget}
-                <input type="submit" value="Enviar">
-            
-        |]
+        addScript $ StaticR js_jquery_min_js
+        addScript $ StaticR js_bootstrap_min_js
+        toWidget $ $(whamletFile "templates/menu.hamlet")
+        toWidget $ $(whamletFile "templates/cadastrar.hamlet") 
 
 postCadLivroR :: Handler Html
 postCadLivroR = do 
     ((result,_),_) <- runFormPost formLivro
     case result of
         FormSuccess livro -> do 
-            runDB $ insert livro 
+            runDB $ insert livro
             redirect ListarR
-        _ -> redirect HomeR
+        _ -> do
+            setMessage $ [shamlet| Dados invalidos! |] 
+            redirect CadLivroR
+            
