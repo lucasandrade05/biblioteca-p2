d2b36ba0ef2328688028b4a2d7e9cf0036aa641e
romefeller <algarcia@usp.br>
2017-11-28 21:32:08 +0000

    Adicionado pagina detalhes do livro

--- src/Handler/Listar.hs
+++ src/Handler/Listar.hs
@@ -27,7 +27,11 @@ data Pesquisa = Pesquisa
 
 formPesquisa :: Form Pesquisa
 formPesquisa = renderBootstrap $ Pesquisa
-    <$> areq textField "" Nothing
+        <$> areq textField FieldSettings{fsId=Just "search",
+                           fsLabel="Titulo: ",
+                           fsTooltip= Nothing,
+                           fsName= Nothing,
+                           fsAttrs=[("class","form-control")]} Nothing
     
 toTexto :: Pesquisa -> Text
 toTexto (Pesquisa x) = x
@@ -42,33 +46,6 @@ getListarR = do
         addScript $ StaticR js_bootstrap_min_js
         toWidget $ $(whamletFile "templates/menu.hamlet")
         toWidget $ $(whamletFile "templates/listar.hamlet") 
-        
-
-
-postPesqLivroR :: Handler Html
-postPesqLivroR = do 
-    ((result,_),_) <- runFormPost formPesquisa
-    case result of
-        FormSuccess pesquisar -> do 
-            let livro = toTexto(pesquisar)
-            redirect (BuscarLivroR livro)
-        _ -> do
-            setMessage $ [shamlet| Dados invalidos! |] 
-            redirect ListarR
-            
-getBuscarLivroR :: Text -> Handler Html
-getBuscarLivroR livro = do
-    (widget, enctype) <- generateFormPost formPesquisa
-    livros <- runDB $ selectList [Filter LivroTitulo (Left $ "%"++ livro ++"%") (BackendSpecificFilter "ILIKE")] []
-    --livros <- runDB $ selectList [LivroTitulo >. "%"++livros++"%"][Asc LivroAutor, Asc LivroTitulo]
-    defaultLayout $ do 
-        addStylesheet $ (StaticR css_bootstrap_css)
-        addScript $ StaticR js_jquery_min_js
-        addScript $ StaticR js_bootstrap_min_js
-        toWidget $ $(whamletFile "templates/menu.hamlet")
-        toWidget $ $(whamletFile "templates/listar.hamlet") 
-
-
         
         
 postApagarLivroR :: LivroId -> Handler Html 
@@ -90,10 +67,52 @@ postCadLivroR :: Handler Html
 postCadLivroR = do 
     ((result,_),_) <- runFormPost formLivro
     case result of
-        FormSuccess livro -> do 
+        FormSuccess livro -> do
             runDB $ insert livro
             redirect ListarR
         _ -> do
             setMessage $ [shamlet| Dados invalidos! |] 
             redirect CadLivroR
+
+getDetalheLivroR :: LivroId -> Handler Html
+getDetalheLivroR livro = do
+    (widget, enctype) <- generateFormPost formPesquisa
+    --livros <- runDB $ selectList [] [Asc LivroAutor, Asc LivroTitulo]
+    defaultLayout $ do 
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addScript $ StaticR js_jquery_min_js
+        addScript $ StaticR js_bootstrap_min_js
+        toWidget $ $(whamletFile "templates/menu.hamlet")
+        toWidget $ $(whamletFile "templates/livro.hamlet")
+
+
+
+
+
+postPesqLivroR :: Handler Html
+postPesqLivroR = do 
+    ((result,_),_) <- runFormPost formPesquisa
+    case result of
+        FormSuccess pesquisar -> do 
+            let livro = toTexto(pesquisar)
+            redirect (BuscarLivroR livro)
+        _ -> do
+            setMessage $ [shamlet| Dados invalidos! |] 
+            redirect ListarR
             
+getBuscarLivroR :: Text -> Handler Html
+getBuscarLivroR livro = do
+    (widget, enctype) <- generateFormPost formPesquisa
+    livros <- runDB $ selectList ([Filter LivroTitulo (Left $ "%"++ livro ++"%") (BackendSpecificFilter "ILIKE")]
+                              ||.[Filter LivroAutor  (Left $ "%"++ livro ++"%") (BackendSpecificFilter "ILIKE")]
+                              ||.[Filter LivroEditora(Left $ "%"++ livro ++"%") (BackendSpecificFilter "ILIKE")])[]
+    --livros <- runDB $ selectList [LivroTitulo >. "%"++livros++"%"][Asc LivroAutor, Asc LivroTitulo]
+    defaultLayout $ do 
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addScript $ StaticR js_jquery_min_js
+        addScript $ StaticR js_bootstrap_min_js
+        toWidget $ $(whamletFile "templates/menu.hamlet")
+        toWidget $ $(whamletFile "templates/listar.hamlet") 
+        [whamlet| 
+            <div class="col-sm-6"> <i>Exibindo resultados para "#{livro}" 
+        |]
